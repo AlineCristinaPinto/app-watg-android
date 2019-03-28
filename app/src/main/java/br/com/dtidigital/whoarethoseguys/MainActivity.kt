c03af781bc146509ae1635108a72c8f1975e45f7
Aline Cristina Pinto <aline.cpinto@dtidigital.com.br>
2019-03-28 17:20:49 -0300

    Adicionando call para service

--- app/src/main/java/br/com/dtidigital/whoarethoseguys/MainActivity.kt
+++ app/src/main/java/br/com/dtidigital/whoarethoseguys/MainActivity.kt
@@ -1,11 +1,16 @@
 package br.com.dtidigital.whoarethoseguys
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import br.com.dtidigital.whoarethoseguys.adapter.RecycleViewAdapter
 import br.com.dtidigital.whoarethoseguys.model.entities.Tribo
+import br.com.dtidigital.whoarethoseguys.retrofit.RetrofitInitializer
 import kotlinx.android.synthetic.main.activity_main_host.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,13 +18,37 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_host)
 
+        //callListaTribos()
+        configuraListagemTribos()
+    }
+
+    /*private fun callListaTribos(){
+
+        val call = RetrofitInitializer().triboService().listaTribos()
+
+        call.enqueue(object : Callback<List<Tribo>?> {
+            override fun onResponse(call: Call<List<Tribo>?>?,
+                                    response: Response<List<Tribo>?>?) {
+                response?.body()?.let {
+                    val tribos: List<Tribo> = it
+                    configuraListagemTribos(tribos)
+
+                }
+            }
+
+            override fun onFailure(call: Call<List<Tribo>?>?, t: Throwable?) {
+                Log.e("onFailure error", t?.message)
+            }
+        })
+    }*/
+
+    private fun configuraListagemTribos(){
         val recyclerView = recyclerViewTribo
 
         recyclerView.adapter = RecycleViewAdapter(tribos(), imagensTribos(), this)
 
         val layoutManager = StaggeredGridLayoutManager(1, StaggeredGridLayoutManager.VERTICAL)
         recyclerView.layoutManager = layoutManager
-
     }
 
     private fun tribos() : List<Tribo>{
